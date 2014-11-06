d9b3efaeafde2372acaf17e2e5908636efbc4b98
Nick Parfene <nick@tmro.net>
2014-11-06 19:13:44 +1300

    It scans
    Yes it does

--- MagicCardWall/MagicCardWall/FirstViewController.m
+++ MagicCardWall/MagicCardWall/FirstViewController.m
@@ -6,9 +6,13 @@
 //  Copyright (c) 2014 Trade Me. All rights reserved.
 //
 
+@import AVFoundation;
+
 #import "FirstViewController.h"
 
-@interface FirstViewController ()
+@interface FirstViewController () <AVCaptureMetadataOutputObjectsDelegate>
+
+@property (strong) AVCaptureSession *captureSession;
 
 @end
 
@@ -22,6 +26,54 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)touchedButtonScan:(UIButton *)sender {
+    NSLog(@"Touched button scan");
+    
+    [self scanQRCode];
+}
+
+- (void)scanQRCode {
+    self.captureSession = [[AVCaptureSession alloc] init];
+    AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    NSError *error = nil;
+    
+    AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:device
+                                                                        error:&error];
+    if (input) {
+        [self.captureSession addInput:input];
+    } else {
+        NSLog(@"Error: %@", error);
+    }
+    
+    AVCaptureMetadataOutput *metadataOutput = [[AVCaptureMetadataOutput alloc] init];
+    [self.captureSession addOutput:metadataOutput];
+    [metadataOutput setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
+    [metadataOutput setMetadataObjectTypes:@[AVMetadataObjectTypeQRCode, AVMetadataObjectTypeEAN13Code]];
+    
+    AVCaptureVideoPreviewLayer *previewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:self.captureSession];
+    previewLayer.frame = self.view.layer.bounds;
+    [self.view.layer addSublayer:previewLayer];
+    
+    [self.captureSession startRunning];
+}
+
+#pragma mark - AVCaptureMetadataOutputObjectsDelegate
+
+- (void)captureOutput:(AVCaptureOutput *)captureOutput
+didOutputMetadataObjects:(NSArray *)metadataObjects
+       fromConnection:(AVCaptureConnection *)connection {
+    NSString *QRCode = nil;
+    for (AVMetadataObject *metadata in metadataObjects) {
+        if ([metadata.type isEqualToString:AVMetadataObjectTypeQRCode]) {
+            // This will never happen; nobody has ever scanned a QR code... ever
+            QRCode = [(AVMetadataMachineReadableCodeObject *)metadata stringValue];
+            break;
+        }
+    }
+    
+    NSLog(@"QR Code: %@", QRCode);
 }
 
 @end
