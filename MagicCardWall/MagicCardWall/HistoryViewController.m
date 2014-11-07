71fd2d5fce93ac0bb29b868c04d2e8a5d788634d
Nick Parfene <nick@tmro.net>
2014-11-07 14:42:26 +1300

    Added history cells

--- MagicCardWall/MagicCardWall/HistoryViewController.m
+++ MagicCardWall/MagicCardWall/HistoryViewController.m
@@ -11,6 +11,17 @@
 #import "MagicCardWallClient.h"
 #import "HistoryItem.h"
 
+@interface HistoryItemCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageViewGravatar;
+@end
+
+@implementation HistoryItemCell
+
+@end
+
+#pragma mark - View Controller
+
 @interface HistoryViewController ()
 
 @property (weak, nonatomic) IBOutlet UILabel *labelShakeIt;
@@ -24,12 +35,19 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    self.tableView.rowHeight = 120;
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    
     [[MagicCardWallClient sharedInstance] getHistoryWithCompletion:^(NSArray *arrayOfHistoryItems, NSError *error) {
         if (error) {
             NSLog(@"Error getting the history items %@", [error localizedDescription]);
         }
         else {
-            NSLog(@"Got %ul items", [arrayOfHistoryItems count]);
+            NSLog(@"Got %i items", [arrayOfHistoryItems count]);
             self.arrayOfHistoryItems = arrayOfHistoryItems;
             
             [self.tableView reloadData];
@@ -37,9 +55,18 @@
     }];
 }
 
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.arrayOfHistoryItems count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
